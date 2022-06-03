f49ce48f56722fe0bccb110d52be1f9547bfb99e
Ralf <ralflopez3@gmail.com>
2022-06-03 20:58:31 +0800

    :lipstick: create navbar with 3d rotating links

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import styles from "../styles/Navbar.module.css"
+
+const pages = [
+  { name: "Projects", href: "#" },
+  { name: "About", href: "#about" },
+  { name: "Contact", href: "#contact" },
+]
+
+export const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <p className={styles.brand}>Ralf Lopez </p>
+        <nav>
+          {/* Menu */}
+          <button className={styles.menuBtn}>MENU+</button>
+          {/* Links */}
+          {pages.map((p) => (
+            <a key={p.href} href={p.href} className={styles.menuItem}>
+              <div>
+                {p.name.split("").map((letter, i) => (
+                  <span
+                    key={letter + i}
+                    style={{ "--index": i } as React.CSSProperties}
+                  >
+                    {letter}
+                  </span>
+                ))}
+              </div>
+              <div>
+                {p.name.split("").map((letter, i) => (
+                  <span
+                    key={letter + i}
+                    style={{ "--index": i } as React.CSSProperties}
+                  >
+                    {letter}
+                  </span>
+                ))}
+              </div>
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
