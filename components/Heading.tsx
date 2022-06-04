80797e401a8dad9f07045fb6ce2705eb9e10ea2d
Ralf <ralflopez3@gmail.com>
2022-06-04 21:06:13 +0800

    :lipstick: make skills section

---
+++ components/Heading.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styles from "../styles/Heading.module.scss"
+
+interface Props {
+  title: string
+  caption: string
+}
+
+export const Heading = ({ caption, title }: Props) => {
+  return (
+    <div className={styles.heading}>
+      <p>/ {title}</p>
+      <p>{caption}</p>
+    </div>
+  )
+}
---
+++ icons/index.ts
@@ -0,0 +1,33 @@
+import html from "./html.svg"
+import css from "./css.svg"
+import js from "./js.svg"
+import ts from "./ts.svg"
+import go from "./go.svg"
+import java from "./java.svg"
+import react from "./react.svg"
+import nextjs from "./nextjs.svg"
+import node from "./node.svg"
+import nestjs from "./nestjs.svg"
+import express from "./express.svg"
+import graphql from "./graphql.svg"
+import git from "./git.svg"
+import postgres from "./postgres.svg"
+import docker from "./docker.svg"
+
+export {
+  html,
+  css,
+  js,
+  ts,
+  go,
+  java,
+  react,
+  nextjs,
+  node,
+  nestjs,
+  express,
+  graphql,
+  git,
+  postgres,
+  docker,
+}
