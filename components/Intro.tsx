2563941cedc6fe2c5db7d0bccf3b3697a799a56a
Ralf <ralflopez3@gmail.com>
2022-06-04 16:47:59 +0800

    :recycle: convert css to scss

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import styles from "../styles/Intro.module.css"
+import styles from "../styles/Intro.module.scss"
 
 export const Intro = () => {
   return (
-    <div className={styles.intro}>
+    <section className={styles.intro}>
       <h1>
         <span>A software engineer</span>
         <span className={styles.ball}>
@@ -15,6 +15,6 @@ export const Intro = () => {
         <p>Lets work</p>
         <p>Together</p>
       </div>
-    </div>
+    </section>
   )
 }
