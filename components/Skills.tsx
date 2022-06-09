999e94cd9bb0e42d23183d3a4530d61bc75db62f
Ralf <ralflopez3@gmail.com>
2022-06-09 14:43:11 +0800

    :fire: remove unnecessary skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,41 +2,26 @@ import React, { useRef } from "react"
 import { Heading } from "./Heading"
 import styles from "../styles/Skills.module.scss"
 import {
-  html,
-  css,
   js,
   ts,
-  go,
-  java,
   react,
   nextjs,
   node,
   nestjs,
-  express,
   graphql,
-  git,
   postgres,
   docker,
 } from "../icons"
 import { useReveal } from "../hooks"
 
 const skills = [
-  {
-    name: "HTML",
-    icon: html,
-  },
-  { name: "CSS", icon: css },
   { name: "JAVASCRIPT", icon: js },
   { name: "TYPESCRIPT", icon: ts },
-  { name: "GO", icon: go },
-  { name: "JAVA", icon: java },
   { name: "REACT", icon: react },
   { name: "NEXT.JS", icon: nextjs },
   { name: "NODE", icon: node },
   { name: "NEST.JS", icon: nestjs },
-  { name: "EXPRESS", icon: express },
   { name: "GRAPHQL", icon: graphql },
-  { name: "GIT", icon: git },
   { name: "POSTGRES", icon: postgres },
   { name: "DOCKER", icon: docker },
 ]
